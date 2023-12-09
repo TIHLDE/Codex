1dc23b1e56cc8df4fea7c08232a6c93021570a1b
Anders <anders.morille@gmail.com>
2023-12-09 19:48:30 +0100

    Fixing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { SideNav, TableOfContents, TopNav } from "../components";
 import "prismjs";
 // Import other Prism themes here
 import "prismjs/components/prism-bash.min";
-import "prismjs/themes/prism.css";
+// import "prismjs/themes/prism.css";
 
 import "../public/globals.css";
 import "../public/codeStyle.css";
