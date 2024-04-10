fe0453caa212b636d9e3a52f0f427188efae3118
Anders <anders.morille@gmail.com>
2024-04-10 15:48:24 +0200

    Fix build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
 import authOptions from '../../../../auth/auth';
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
