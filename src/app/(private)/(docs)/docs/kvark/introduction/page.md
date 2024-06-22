550a67c8d836e7f7ae6856c2b88687f7f6276d6d
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-06-22 13:24:13 +0200

    Initial docs

---
+++ src/app/(private)/(docs)/docs/kvark/introduction/page.md
@@ -0,0 +1,9 @@
+---
+title: 'Introduksjon'
+---
+
+Kvark benytter seg i hovedsak av følgende teknologier:
+
+- Vite
+- React
+-
--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -49,45 +49,84 @@ export const navigation: Navigation[] = [
     ],
   },
   {
+    title: 'Kvark',
+    href: '/docs/kvark',
+    children: [
+      {
+        title: 'Introduksjon',
+        href: '/docs/kvark/introduction',
+      },
+      {
+        title: 'Installasjon',
+        href: '/docs/kvark/installation',
+      },
+      {
+        title: 'Et lite eksempel',
+        href: '/docs/kvark/example',
+      },
+      {
+        title: 'How-to',
+        children: [
+          {
+            title: 'Modeller og tabeller i databasen',
+            href: '/docs/kvark/howto/models',
+          },
+          {
+            title: 'Serializere og JSON',
+            href: '/docs/kvark/howto/serializers',
+          },
+          {
+            title: 'Viewsets og respons',
+            href: '/docs/kvark/howto/viewsets',
+          },
+        ],
+      },
+      {
+        title: 'Hvordan håndtere filopplastning',
+        href: '/docs/kvark/extensions/fileUpload',
+      },
+    ],
+  },
+  {
     title: 'Prosjekter',
     href: '/docs/projects',
     children: [
       {
         title: 'Lepton',
-        href: '/docs/projects/lepton'
+        href: '/docs/projects/lepton',
       },
       {
         title: 'Kvark',
-        href: '/docs/projects/kvark'
+        href: '/docs/projects/kvark',
       },
       {
         title: 'Kontres',
-        href: '/docs/projects/kontres'
+        href: '/docs/projects/kontres',
       },
       {
         title: 'Blitzed',
-        href: '/docs/projects/blitzed'
+        href: '/docs/projects/blitzed',
       },
       {
         title: 'Codex',
-        href: '/docs/projects/codex'
+        href: '/docs/projects/codex',
       },
       {
         title: 'Fondet',
-        href: '/docs/projects/fondet'
+        href: '/docs/projects/fondet',
       },
       {
         title: 'Jubileum',
-        href: '/docs/projects/jubileum'
+        href: '/docs/projects/jubileum',
       },
       {
         title: 'Shorty',
-        href: '/docs/projects/shorty'
+        href: '/docs/projects/shorty',
       },
       {
         title: 'ScriptStack',
-        href: '/docs/projects/scriptstack'
-      }
-    ]
+        href: '/docs/projects/scriptstack',
+      },
+    ],
   },
 ];
