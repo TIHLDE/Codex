7816e99a021adb6f04b8c073aa839cba88e7b202
Anders Noël Lothe Morille <anders.morille@gmail.com>
2024-06-22 13:22:21 +0200

    Fixed secondary button in light mode

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,14 @@ import clsx from 'clsx';
 
 const variantStyles = {
   primary:
-    'rounded-lg bg-sky-700 py-2 px-4 text-sm font-semibold text-white hover:bg-sky-200' +
+    'rounded-lg bg-sky-700 py-2 px-4 text-sm font-semibold text-white hover:bg-sky-600' +
     ' focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500',
   secondary:
     ' text-gray-800 rounded-lg ring-1 ring-gray-300 dark:ring-none dark:bg-slate-800 py-2 px-4' +
     ' text-sm' +
     ' font-medium' +
-    ' dark:text-white' +
-    ' hover:bg-slate-100 dark:hover:bg-slate-700 focus:outline-none focus-visible:outline-2' +
+    ' text-white' +
+    ' hover:bg-slate-100 hover:bg-slate-700 focus:outline-none focus-visible:outline-2' +
     ' focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400',
   destructive:
     'rounded-lg bg-red-500 py-2 px-4 text-sm font-medium text-white hover:bg-red-400' +
