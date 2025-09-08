4427b3e0990b0d68fdd67180e2bc75638d283921
Borgar <142639383+borgaar@users.noreply.github.com>
2025-09-08 13:34:14 +0200

    docker image will now be tagged with commit and no caching in prod (#84)

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,14 @@ set -e
 echo "-> Loading env variables from .env file into shell"
 export $(grep -v '^#' .env | xargs)
 
+COMMIT_HASH=$(git rev-parse --short HEAD)
+
 echo "-> Building new Docker image"
-docker build --build-arg NEXT_PUBLIC_ALLOWED_GROUP_SLUGS=$NEXT_PUBLIC_ALLOWED_GROUP_SLUGS -t codex.tihlde.org .
+docker build --no-cache --build-arg NEXT_PUBLIC_ALLOWED_GROUP_SLUGS=$NEXT_PUBLIC_ALLOWED_GROUP_SLUGS -t codex.tihlde.org:$COMMIT_HASH .
 
 echo "-> Stopping and removing old container"
 docker rm -f codex.tihlde.org
 
 echo "-> Starting new container"
-docker run --env-file .env -p 5000:3000 --name codex.tihlde.org --restart unless-stopped -d codex.tihlde.org
+docker run --env-file .env -p 5000:3000 --name codex.tihlde.org --restart unless-stopped -d codex.tihlde.org:$COMMIT_HASH
+
