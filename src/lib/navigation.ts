72aa04729249caf4c3bd0d21857902e683f9f7cc
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2025-10-30 20:53:00 +0100

    added royal and adelie docs

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -249,43 +249,43 @@ export const navigation: Navigation[] = [
         children: [
           {
             title: '0. Azure læringsløp',
-            href: '/docs/drift/azure/azure-laeringslop'
+            href: '/docs/drift/azure/azure-laeringslop',
           },
           {
             title: '1. IaC & Terraform basic',
-            href: '/docs/drift/azure/iac-terraform-basic'
+            href: '/docs/drift/azure/iac-terraform-basic',
           },
           {
             title: '2. Resources & RG',
-            href: '/docs/drift/azure/resources-rg'
+            href: '/docs/drift/azure/resources-rg',
           },
           {
             title: '3. Azure Containers',
-            href: '/docs/drift/azure/azure-containers'
+            href: '/docs/drift/azure/azure-containers',
           },
           {
             title: '4. Azure Cost Management',
-            href: '/docs/drift/azure/azure-cost-management'
+            href: '/docs/drift/azure/azure-cost-management',
           },
           {
             title: '5. Azure VNet',
-            href: '/docs/drift/azure/azure-vnet'
+            href: '/docs/drift/azure/azure-vnet',
           },
           {
             title: '6. Azure DB',
-            href: '/docs/drift/azure/azure-db'
+            href: '/docs/drift/azure/azure-db',
           },
           {
             title: '7. Azure Storage Account',
-            href: '/docs/drift/azure/azure-storage-account'
+            href: '/docs/drift/azure/azure-storage-account',
           },
           {
             title: '8. Terraform Intermediate',
-            href: '/docs/drift/azure/terraform-intermediate'
+            href: '/docs/drift/azure/terraform-intermediate',
           },
           {
             title: '9. Azure Container Registry',
-            href: '/docs/drift/azure/azure-container-registry'
+            href: '/docs/drift/azure/azure-container-registry',
           },
         ],
       },
@@ -297,15 +297,19 @@ export const navigation: Navigation[] = [
             href: '/docs/drift/openstack/installation',
           },
           {
-            title: 'Virtuelle maskiner',
+            title: 'Opprettelse med Web',
+            href: '/docs/drift/openstack/vm/web',
+          },
+          {
+            title: 'Drifts VM-er',
             children: [
               {
-                title: 'Opprettelse med CLI',
-                href: '/docs/drift/openstack/vm/cli',
+                title: 'Adelie',
+                href: '/docs/drift/openstack/instances/adelie',
               },
               {
-                title: 'Opprettelse med Web',
-                href: '/docs/drift/openstack/vm/web',
+                title: 'Royal',
+                href: '/docs/drift/openstack/instances/royal',
               },
             ],
           },
@@ -350,7 +354,7 @@ export const navigation: Navigation[] = [
           {
             title: 'Deploy',
             href: '/docs/drift/linux/deploy',
-          }
+          },
         ],
       },
       {
